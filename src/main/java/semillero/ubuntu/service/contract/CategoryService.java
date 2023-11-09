0904d79532f8d688b7cff1d2aba316f7339dd468
andres briñez <brinezlopez08@gmail.com>
2023-11-08 21:45:32 -0500

    Se crea el método para crear una categoria

--- src/main/java/semillero/ubuntu/service/contract/CategoryService.java
+++ src/main/java/semillero/ubuntu/service/contract/CategoryService.java
@@ -8,11 +8,22 @@ public interface CategoryService {
 
     /*
      *
+     * Crea una nueva categoría.
+     * @param category La categoría a crear.
+     * @return La categoría creada.
+     *
+     */
+    Category createCategory(Category category);
+
+    /*
+     *
      * Obtiene una lista de todas las Categorías existentes.
      * @return Una lista de objetos Category que representan todas las categorías disponibles.
      *
      */
     List<Category> getAllCategories();
+
+
 
 
 }
