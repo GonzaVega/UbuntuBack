9a2090bb47f3435223dc6dbb174a098743177ba6
andres briñez <brinezlopez08@gmail.com>
2023-11-08 22:16:19 -0500

    Se crea el método para obtener la categoria por nombre

--- src/main/java/semillero/ubuntu/repository/CategoryRepository.java
+++ src/main/java/semillero/ubuntu/repository/CategoryRepository.java
@@ -1,9 +1,14 @@
 package semillero.ubuntu.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import semillero.ubuntu.entities.Category;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    // Encontrar una categoría por nombre con sentencia SQL
+
+     Category findByNombre(String nombre);
 }
