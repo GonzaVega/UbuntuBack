afaf4c27f36fa5ae51a102fe7b9afd1331bbb57e
andres briñez <brinezlopez08@gmail.com>
2023-11-08 21:19:41 -0500

    Se crea la clase service que implementa la interfaz

---
+++ src/main/java/semillero/ubuntu/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,33 @@
+package semillero.ubuntu.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.stereotype.Service;
+import semillero.ubuntu.entities.Category;
+import semillero.ubuntu.repository.CategoryRepository;
+import semillero.ubuntu.service.contract.CategoryService;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    // Inyección de dependencias a través del constructor
+    private final CategoryRepository categoryRepository;
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        // Si no hay categorías registradas, lanza una excepción
+        if (categoryRepository.count() == 0) {
+            throw new ResourceNotFoundException("No categories found");
+        }
+
+        // Ordenar la lista de categorías por nombre
+        //Collections.sort(categories, (c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
+
+        return categoryRepository.findAll();
+    }
+}
