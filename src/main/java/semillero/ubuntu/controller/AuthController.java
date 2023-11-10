87a3920c8229f8c4b8c0bcd0dc6589683a559cc4
j-evan <monzonjorge789@gmail.com>
2023-11-10 12:29:07 -0300

    creación de seguridad

--- src/main/java/semillero/ubuntu/controller/AuthController.java
+++ src/main/java/semillero/ubuntu/controller/AuthController.java
@@ -1,4 +1,22 @@
 package semillero.ubuntu.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
 public class AuthController {
+
+    @GetMapping("/")
+    public String home(){
+        return "Hello, home";
+    }
+
+    @GetMapping("/secured")
+    public String secured(){
+        return "Hello, secured";
+    }
+
 }
