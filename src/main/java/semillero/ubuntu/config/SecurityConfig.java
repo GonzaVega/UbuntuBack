db3cd7ec0e7590267edd326af583f6ee19cae255
karen <quemojojojo@gmail.com>
2023-11-15 00:11:40 -0500

    initial test

--- src/main/java/semillero/ubuntu/config/SecurityConfig.java
+++ src/main/java/semillero/ubuntu/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -16,13 +17,15 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> {
-            auth.requestMatchers("/").permitAll();
-                    auth.requestMatchers("/admin").hasRole("ADMIN");
-                    auth.requestMatchers("/user").hasAnyRole("USER","ADMIN");
+            auth.requestMatchers("/**").permitAll();
+                    //auth.requestMatchers("/admin").hasRole("ADMIN");
+                    //auth.requestMatchers("/user").hasAnyRole("USER","ADMIN");
             auth.anyRequest().authenticated();
         })
-                .oauth2Login(withDefaults())
+                //.oauth2Login(withDefaults())
+                .oauth2Login(OAuth2LoginConfigurer::disable)  // Deshabilita completamente la autenticación OAuth2
                 .formLogin(withDefaults())
+                .formLogin(login -> login.disable())
                 .build();
     }
 }
