6882e0b77f72622024e6c11e3ca4cf6dc1d4ae9f
andres briñez <brinezlopez08@gmail.com>
2023-11-27 21:44:26 -0500

    Add findByIdWithImagenes method to MicroentrepreneurshipRepository

--- src/main/java/semillero/ubuntu/repository/MicroentrepreneurshipRepository.java
+++ src/main/java/semillero/ubuntu/repository/MicroentrepreneurshipRepository.java
@@ -2,10 +2,17 @@ package semillero.ubuntu.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import semillero.ubuntu.entities.Microentrepreneurship;
 
+import java.util.Optional;
+
 @Repository
 public interface MicroentrepreneurshipRepository extends JpaRepository<Microentrepreneurship, Long> {
-    
+    // Toca de esta manera  por la lazy loading, 
+    @Query("SELECT m FROM Microentrepreneurship m LEFT JOIN FETCH m.imagenes WHERE m.id = :id")
+    Optional<Microentrepreneurship> findByIdWithImagenes(@Param("id") Long id);
+
 }
