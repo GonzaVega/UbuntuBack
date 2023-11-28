df8e893377dfc4c0177fd715ae7774d2ac3fb4b0
andres briñez <brinezlopez08@gmail.com>
2023-11-28 00:22:20 -0500

    Update editMicroentrepreneurship method signature

--- src/main/java/semillero/ubuntu/service/contract/MicroentrepreneurshipService.java
+++ src/main/java/semillero/ubuntu/service/contract/MicroentrepreneurshipService.java
@@ -5,7 +5,7 @@ import semillero.ubuntu.entities.Microentrepreneurship;
 public interface MicroentrepreneurshipService {
 
     Microentrepreneurship createMicroentrepreneurship(Microentrepreneurship microentrepreneurship);
-    Microentrepreneurship editMicroentrepreneurship(Long id);
+    Microentrepreneurship editMicroentrepreneurship(Long id, Microentrepreneurship microentrepreneurship);
     void hideMicroentrepreneurship(Long id);
     // Obtener microemprendimientos por id
     Microentrepreneurship getMicroentrepreneurshipById(Long id);
