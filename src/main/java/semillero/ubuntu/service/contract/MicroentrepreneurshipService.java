81804f9ea1fc928efd6d1577a07062b2864b5b7b
andres briñez <brinezlopez08@gmail.com>
2023-12-12 11:43:54 -0500

    Add manageMicroentrepreneurship method

--- src/main/java/semillero/ubuntu/service/contract/MicroentrepreneurshipService.java
+++ src/main/java/semillero/ubuntu/service/contract/MicroentrepreneurshipService.java
@@ -9,6 +9,9 @@ public interface MicroentrepreneurshipService {
     Microentrepreneurship createMicroentrepreneurship(Microentrepreneurship microentrepreneurship);
     Microentrepreneurship editMicroentrepreneurship(Long id, Microentrepreneurship microentrepreneurship);
     void hideMicroentrepreneurship(Long id);
+    // gestionar microemprendimiento
+    void manageMicroentrepreneurship(Long id);
+
     // Obtener microemprendimientos por id
     Microentrepreneurship getMicroentrepreneurshipById(Long id);
 
